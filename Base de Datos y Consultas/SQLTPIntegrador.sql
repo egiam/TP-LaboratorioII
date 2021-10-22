c8de0fd28dd259f9cb5d2533f1350ec11e82b0f3
Ezequiel <ezegiampaoli@gmail.com>
2021-10-22 11:30:07 -0300

    Algunos Insert

--- Base de Datos y Consultas/SQLTPIntegrador.sql
+++ Base de Datos y Consultas/SQLTPIntegrador.sql
@@ -208,7 +208,7 @@ create table detalles_factura
 	precio_unitario decimal(10,2),
 	cantidad int,
 	descuento decimal(5,3),
-	resembolsado bit, --Bool
+	reembolsado bit, --Bool
 	codigo_barra int foreign key
 	References suministros(codigo_barra)
 )
@@ -276,8 +276,165 @@ create table descuentos
 -- Insertar 
 
 
+--PROVINCIAS
+INSERT INTO PROVINCIAS (provincia) VALUES('CORDOBA')
+INSERT INTO PROVINCIAS (provincia) VALUES('BUENOS AIRES')
+INSERT INTO PROVINCIAS (provincia) VALUES('CATAMARCA')
+INSERT INTO PROVINCIAS (provincia) VALUES('CHACO')
+INSERT INTO PROVINCIAS (provincia) VALUES('CHUBUT')
+INSERT INTO PROVINCIAS (provincia) VALUES('JUJUY')
+INSERT INTO PROVINCIAS (provincia) VALUES('CORRIENTES')
+INSERT INTO PROVINCIAS (provincia) VALUES('ENTRE RIOS')
+INSERT INTO PROVINCIAS (provincia) VALUES('FORMOSA')
+INSERT INTO PROVINCIAS (provincia) VALUES('LA PAMPA')
+INSERT INTO PROVINCIAS (provincia) VALUES('LA RIOJA')
+INSERT INTO PROVINCIAS (provincia) VALUES('MENDOZA')
+INSERT INTO PROVINCIAS (provincia) VALUES('MISIONES')
+INSERT INTO PROVINCIAS (provincia) VALUES('NEUQUEN')
+INSERT INTO PROVINCIAS (provincia) VALUES('RIO NEGRO')
+INSERT INTO PROVINCIAS (provincia) VALUES('SALTA')
+INSERT INTO PROVINCIAS (provincia) VALUES('SAN JUAN')
+INSERT INTO PROVINCIAS (provincia) VALUES('SAN LUIS')
+INSERT INTO PROVINCIAS (provincia) VALUES('SANTA CRUZ')
+INSERT INTO PROVINCIAS (provincia) VALUES('SANTA FE')
+INSERT INTO PROVINCIAS (provincia) VALUES('SANTIAGO DEL ESTERO')
+INSERT INTO PROVINCIAS (provincia) VALUES('TIERRA DEL FUEGO')
+INSERT INTO PROVINCIAS (provincia) VALUES('TUCUMAN')
+
+
+--LOCALIDADES
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CORDOBA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CARLOS PAZ',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ADELIA MARIA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ALTA GRACIA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('AGUA DE ORO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('AGUA DEL TALA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('AGUA PINTADA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ALEJANDRO ROCA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ARIAS',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ARROYO CABRAL',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ARROYO LA HIGUERA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('ASCOCHINGA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BAJO DE FERNANDEZ',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BAJO DEL CARMEN',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BAJO GRANDE',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BAJO HONDO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BAJO LINDO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BARRIO DEAN FUNES',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BARRIO LA FERIA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('BO FLORES GUARNICION AEREA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAJON DEL RIO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CALASUYA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAMPO BANDILLO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAMPO LOS ZORROS',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CANDELARIA SUR',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAPILLA DE SITON',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAPILLA LA ESPERANZA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CASAS VEJAS',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CASEROS ESTE',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAÑADA DE CUEVAS',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAÑADA DEL TALA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CAÑADA VERDE',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CERRO PELADO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CHARACATO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CHAÑARIACO',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CHIPITIN',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CHUA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('CHURQUI CAÑADA',1)
+INSERT INTO LOCALIDADES (localidad, cod_provincia) VALUES('COLAZO',1)
+
+
+--BARRIOS
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('CENTRO',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('ALTO ALBERDI',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('OBSERVATORIO',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('JARDIN',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('GENERAL PAZ',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('PUEYRREDON',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('PARQUE HORIZONTE',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('SAN MARTIN',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('SAN VICENTE',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('JUNIOR',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('MAIPU',1)
+INSERT INTO BARRIOS(barrio, cod_localidad) VALUES ('PANAMERICANO',1)
+
+--tipos contactos
+INSERT INTO tipo_contactos(tipo_contacto) values('Celular')
+INSERT INTO tipo_contactos(tipo_contacto) values('E-mail')
+INSERT INTO tipo_contactos(tipo_contacto) values('Instagram')
+INSERT INTO tipo_contactos(tipo_contacto) values('Twitter')
+INSERT INTO tipo_contactos(tipo_contacto) values('Linkedin')
+INSERT INTO tipo_contactos(tipo_contacto) values('Facebook')
+INSERT INTO tipo_contactos(tipo_contacto) values('Fijo')
+
+
+--Clientes  --LE FALTA DATOS
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('ALONSO','CLAUDIO', 'Sarmiento', 1200, 3, 4433223, 2);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('ALVAREZ','MARCELO', 'San Juan', 550, 7, 2112122, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('ALVAREZ','CRISTINA', 'San Roque', 425, 9, 22121112, 3);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('ALVAREZ','CLAUDIO', 'Roure', 900, 4, 987654, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('CAMPOS','RAUL', 'San Martin', 850, 7, 3456234, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('DURAN','CARLA', 'Belgrano', 850,3, 3467834, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('PEREZ','MARIA', 'Monserrat', 850,6, 148625, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('ZAMORA','CARLOS LUIS', 'Manantiales', 850,7, 4865565, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('PEREZ','ANA', 'Avellaneda', 850,1, 8462451, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('PEREA','LUIS M.', 'Don Bosco', 850,6, 75625855, 1);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('PONCE','CARMELO', 'Colon', 850,4, 234565432, 2);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('GALES','JULIA', 'Colon', 850,4, 74565545, 2);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('SUAREZ','PAOLA', 'Colon', 850,4, 85555455, 3);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('MARANINI','JOSE', 'Colon', 850,4, 238955432, 2);
+INSERT INTO CLIENTES (apellido,nombre,calle,nro_calle,cod_barrio,nro_doc,id_tipo_doc,id_plan,fecha_nac,id_tipo_sexo) VALUES ('FONCECA','BERTA', 'Alverdi', 850,2, 4785565, 3);
+
+
+--contactos --FALTAN DATOS
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('154784247',1,15,null);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('lupom@gmail.com',2,null,2);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('@cris.alvarez',3,9,null);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('@m.alvarez',4,2,null);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('158479254',1,null,5);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('j.gales@hotmail.com',2,12,null);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('4579821',7,null,7);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('155248475',1,null,3);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('rcampos@gmail.com',2,5,null);
+insert into contactos (contacto,id_tipo_contacto,id_cliente,id_empleado,id_empresas_log,id_sucursal) values('156123421',1,null,8);
+
+
+
+--facturas
+
+set dateformat dmy
+
+insert into facturas(fecha,id_cliente,id_empleado) values('16/12/2018',7,1)
+insert into facturas(fecha,id_cliente,id_empleado) values('26/06/2020',6,2)
+insert into facturas(fecha,id_cliente,id_empleado) values('18/02/2021',5,1)
+insert into facturas(fecha,id_cliente,id_empleado) values('13/09/2015',4,2)
+insert into facturas(fecha,id_cliente,id_empleado) values('19/03/2019',3,1)
+insert into facturas(fecha,id_cliente,id_empleado) values('09/10/2020',2,2)
+insert into facturas(fecha,id_cliente,id_empleado) values('03/10/2021',1,1)
+insert into facturas(fecha,id_cliente,id_empleado) values('29/07/2015',4,2)
+insert into facturas(fecha,id_cliente,id_empleado) values('07/08/2016',1,1)
+insert into facturas(fecha,id_cliente,id_empleado) values('31/12/2020',7,2)
+
+
+--detalles
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(2,1,5480.00,30)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(3,2,3590.00,23)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(4,3,10980.00,3)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(5,4,1580.00,4)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(6,5,2290.00,45)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(7,6,4500.00,2)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(8,7,12570.00,1)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(9,8,15740.00,22)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(10,9,7250.00,11)
+insert into detalles_factura(id_factura,codigo_barra,precio_unitario,cantidad,descuento,reembolsado) values(1,10,6780.00,12)
+
+
+
+--drop database Farmaceutica
 
 -- Consultas
+
+
 
 
 
