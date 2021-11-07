f1be01bfbfee83e1e71b7637cdeadb9444b98579
Ezequiel <ezegiampaoli@gmail.com>
2021-11-07 19:43:42 -0300

    Se Junto todo

---
+++ VS/TPLaboratorio2/TPLaboratorio2/Tablas/FrmLogistica.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPLaboratorio2.Tablas
+{
+    public partial class FrmLogistica : Form
+    {
+        public FrmLogistica()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogistica_Load(object sender, EventArgs e)
+        {
+            string select = "select id_empresas_log";
+            grdES.DataSource = consultarTabla(select);
+        }
+
+        SqlConnection conexion = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=Farmaceutica;Integrated Security=True");
+        SqlCommand comando = new SqlCommand();
+        DataTable tabla = new DataTable();
+
+        private DataTable consultarTabla(string select)
+        {
+            conexion.Open();
+
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = select;
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+
+            conexion.Close();
+
+            return tabla;
+        }
+    }
+}
