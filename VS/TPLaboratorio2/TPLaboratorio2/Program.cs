4f1fecc0568f315ef5f6600717406e49c936dec4
Ezequiel <ezegiampaoli@gmail.com>
2021-11-06 23:31:33 -0300

    Agregado total

--- VS/TPLaboratorio2/TPLaboratorio2/Program.cs
+++ VS/TPLaboratorio2/TPLaboratorio2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ModernGUI_V3;
 
 namespace TPLaboratorio2
 {
@@ -16,7 +17,7 @@ namespace TPLaboratorio2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmClientes2());
+            Application.Run(new FormPrincipal());
         }
     }
 }
